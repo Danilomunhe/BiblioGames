87b3aa70b8bfae23a17581a0062b5fa64463d3b4
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-04-26 16:49:31 -0300

    começando o insert

--- CMS/model/bd/produto.php
+++ CMS/model/bd/produto.php
@@ -9,6 +9,39 @@
      //import conexão
      require_once('conexaoBD.php');
 
+     function insertProduto($dadosProduto){
+
+        //iniciando a variavel de resposta
+        $statusResposta = (boolean) false;
+
+        //abre a conexão com o banco
+        $conexao = conexaoMySql();
+
+          //Monta o script para enviar para o banco de dados
+          $sql = "insert into tblcontatos(nome, 
+                                        preco, 
+                                        desconto, 
+                                        descricao, 
+                                        foto)
+                                    values('".$dadosProduto['nome']."', 
+                                    '".$dadosProduto['preco']."', 
+                                    '".$dadosProduto['desconto']."', 
+                                    '".$dadosProduto['descricao']."', 
+                                    '".$dadosProduto['foto']."');";
+
+                //Executa o script no BD
+                //Valiação para verificar se o script sql está correto
+                if (mysqli_query($conexao, $sql)){
+                //Validação para verificar se uma linha foi acescentado no BD
+                if(mysqli_affected_rows($conexao))
+                $statusResposta = true;
+                }
+
+            //Solicita o fechamento da conexão com o bd
+            encerrarConexao($conexao);
+
+            return $statusResposta;
+     }
      function selectAllProdutos(){
          //abre a conexao
          $conexao = conexaoMySql();
